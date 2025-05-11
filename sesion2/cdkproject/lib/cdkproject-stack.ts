8395a52c885f4bcf441c3d14854b2c794196073b
Jose Luis Gil <joseluisgilgil@gmail.com>
2025-05-11 10:03:31 +0000

    Cambio para dejar codigo de bajo nivel con reglas de gs separadas

--- sesion2/cdkproject/lib/cdkproject-stack.ts
+++ sesion2/cdkproject/lib/cdkproject-stack.ts
@@ -29,7 +29,7 @@ export class CdkprojectStack extends cdk.Stack {
     const subnet = new ec2.CfnSubnet(this, 'MyPublicSubnet', {
       vpcId: vpc.ref,
       cidrBlock: '10.0.0.0/24',
-      availabilityZone: 'us-east-1a', // Ponemos una AZ fija para evitar lookup
+      availabilityZone: 'us-east-1a',
       mapPublicIpOnLaunch: true,
       tags: [{ key: 'Name', value: 'MyPublicSubnet' }],
     });
@@ -40,44 +40,45 @@ export class CdkprojectStack extends cdk.Stack {
       tags: [{ key: 'Name', value: 'MyRouteTable' }],
     });
 
-    // Asociar Route Table a Subnet
     new ec2.CfnSubnetRouteTableAssociation(this, 'MySubnetAssociation', {
       subnetId: subnet.ref,
       routeTableId: routeTable.ref,
     });
 
-    // Crear ruta por defecto 0.0.0.0/0 hacia el IGW
     new ec2.CfnRoute(this, 'DefaultRoute', {
       routeTableId: routeTable.ref,
       destinationCidrBlock: '0.0.0.0/0',
       gatewayId: igw.ref,
     });
 
-    // Crear Security Group
+    // Crear Security Group sin reglas inline
     const securityGroup = new ec2.CfnSecurityGroup(this, 'InstanceSG', {
       vpcId: vpc.ref,
       groupDescription: 'Allow SSH and HTTP access',
-      securityGroupIngress: [
-        {
-          ipProtocol: 'tcp',
-          fromPort: 22,
-          toPort: 22,
-          cidrIp: '0.0.0.0/0',
-        },
-        {
-          ipProtocol: 'tcp',
-          fromPort: 80,
-          toPort: 80,
-          cidrIp: '0.0.0.0/0',
-        },
-      ],
       tags: [{ key: 'Name', value: 'InstanceSG' }],
     });
 
-    // Crear Instancia EC2
+    // Reglas como recursos separados
+    new ec2.CfnSecurityGroupIngress(this, 'SGRuleSSH', {
+      groupId: securityGroup.ref,
+      ipProtocol: 'tcp',
+      fromPort: 22,
+      toPort: 22,
+      cidrIp: '0.0.0.0/0',
+    });
+
+    new ec2.CfnSecurityGroupIngress(this, 'SGRuleHTTP', {
+      groupId: securityGroup.ref,
+      ipProtocol: 'tcp',
+      fromPort: 80,
+      toPort: 80,
+      cidrIp: '0.0.0.0/0',
+    });
+
+    // Instancia EC2
     new ec2.CfnInstance(this, 'Instance', {
       instanceType: 't2.micro',
-      imageId: 'ami-0e449927258d45bc4', // Amazon Linux 2023 AMI en us-east-1
+      imageId: 'ami-0e449927258d45bc4',
       subnetId: subnet.ref,
       securityGroupIds: [securityGroup.ref],
       userData: cdk.Fn.base64(`#!/bin/bash
@@ -94,4 +95,3 @@ echo "<?php phpinfo(); ?>" > /var/www/html/index.php
     });
   }
 }
-
